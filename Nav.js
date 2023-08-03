2793a7129c0189b6290253c629f8fb12c93e8ca9
vishwajit-dabhi <141336080+vishwajit-dabhi@users.noreply.github.com>
2023-08-03 19:11:01 +0530

    Update Nav.js

--- Nav.js
+++ Nav.js
@@ -1,18 +1,1 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
 
-
-const Nav=()=>{
-    return(
-        <div>
-            <ul className='nav-ul'>
-                <li><Link to="/"> home page</Link></li>
-                <li><Link to="/content">content</Link></li>
-                <li><Link to="/about">about us</Link></li>
-                <li><Link to="/signUp">signUp</Link> </li>
-            </ul>
-        </div>
-    )
-}
-
-export default Nav;
